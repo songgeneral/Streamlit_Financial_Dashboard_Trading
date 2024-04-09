44789fd1faa35d2bb3a031e6c923e76f5e40efe3
cplooo <cplo@kimo.com>
2024-04-10 01:27:33 +0800

    Add files via upload

--- financial_dashboard_trading.py
+++ financial_dashboard_trading.py
@@ -50,9 +50,9 @@ df_original = df_original.drop('Unnamed: 0',axis=1)
 
 
 ###### 選擇資料區間
-st.subheader("台積電(2330)股價資料: 選擇開始與結束日期, 區間:2022-01-03 至 2022-11-18")
-start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
-end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+st.subheader("台積電(2330)股價資料: 選擇開始與結束日期, 區間:2022-01-01 至 2024-04-09")
+start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-01)', '2022-01-01')
+end_date = st.text_input('選擇結束日期 (日期格式: 2024-04-09)', '2024-04-09')
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
 # 使用条件筛选选择时间区间的数据
